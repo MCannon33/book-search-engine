403bfdece3c6fb9ad291a5640fb575a595d2168f
MCannon33 <marialcannon@gmail.com>
2021-03-25 04:05:02 -0400

    update type

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,8 +2,15 @@ const { User } = require("../models");
 
 const resolvers = {
   Query: {
-    thoughts: async () => {
-      return User.find().sort({ createdAt: -1 });
+    User: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return User.find(params).sort({ createdAt: -1 });
+    },
+    // get a user by username
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select("-__v -password")
+        .populate("savedBooks");
     },
   },
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,17 +3,38 @@ const { gql } = require("apollo-server-express");
 
 // create our typeDefs
 const typeDefs = gql`
-type User {
-  _id: ID 
-  username: String 
-  email: String
-  bookCount: Int
-  savedBooks:[]
-}  
-type Query {
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [savedBooks]
+  }
+  type Book {
+    bookId: Int
+    authors: [authors]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type Auth {
+    token: String
+    user: String
+  }
+  type Query {
     me: [User]
+    me(username: String!): User
   }
 `;
 
 // export the typeDefs
 module.exports = typeDefs;
+
+// thoughts(username: String): [Thought]
+// thought(_id: ID!): Thought
+
+//type Query {
+//books(title: String): [Book]
+//book(_id: ID!): Book
+//}
