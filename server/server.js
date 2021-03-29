579c5c0d12635bdb0de7f18876136388f3c9af95
MCannon33 <marialcannon@gmail.com>
2021-03-28 21:21:38 -0400

    remove errors

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+
+const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require("./utils/auth");
+
 const path = require("path");
 const db = require("./config/connection");
 const routes = require("./routes");
-const { authMiddleware } = require("./utils/auth");
-const { ApolloServer } = require("apollo-server-express");
+
 const { typeDefs, resolvers } = require("./schemas");
 
 const app = express();
